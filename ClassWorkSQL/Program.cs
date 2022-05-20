6ae31892019f693773aebc5d7f24177d83b25213
Евгений Гергелиджиу <Euphor1aVegas@gmail.com>
2022-05-20 21:10:16 +0300

    new command

--- ClassWorkSQL/Program.cs
+++ ClassWorkSQL/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 0; i < 20; i++)
+{
+    Console.Write(i.ToString().PadLeft(10));
+    Console.Write(i + 1.ToString().PadLeft(10));
+    Console.WriteLine();
+}
